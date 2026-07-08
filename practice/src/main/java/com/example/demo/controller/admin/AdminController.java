76d7a0f1d42c938980626ba8d7c0e8a828c7b2e5
Sasaki-Momoka <momosasa39@gmail.com>
2026-07-08 11:02:20 +0900

    progress7

--- practice/src/main/java/com/example/demo/controller/admin/AdminController.java
+++ practice/src/main/java/com/example/demo/controller/admin/AdminController.java
@@ -5,11 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.contact.Contact;
 import com.example.demo.service.contact.ContactService;
+
+import jakarta.transaction.Transactional;
 
 @Controller
 public class AdminController {
@@ -28,18 +32,34 @@ public class AdminController {
 
 	}
 	
-	@GetMapping("/admin/contacts/{id}")
-	public String detailContact(@PathVariable("id") Long id, Model model) {
-	//Contact detail = contactService.getDetailById(id);
-		//System.out.println("Admincontrollにいるよ");
-	
-	
-			model.addAttribute("detail", contactService.getDetailById(id));
-				System.out.println(contactService.getDetailById(id).getClass());
-				System.out.println(contactService.getDetailById(id).get());
-		return "contactDetail";
+	@DeleteMapping("/admin/contacts")
+	@Transactional
+	public String delete(@PathVariable("id") Long id ,Model model) {
+		z
+	return ""
 	}
 	
+	/*	@GetMapping("/admin/contacts/{id}")
+		public String detailContact(@PathVariable("id") Long id, Model model) {
+		//Contact detail = contactService.getDetailById(id);
+			//System.out.println("Admincontrollにいるよ");
+		
+		
+				model.addAttribute("detail", contactService.getDetailById(id));
+					System.out.println(contactService.getDetailById(id).getClass());
+					System.out.println(contactService.getDetailById(id).get());
+			return "contactDetail";
+		}*/
+	@GetMapping("/admin/contacts/{id}")
+	public String detailContact(@PathVariable("id") Long id,Model model) {
+
+		Contact detail = contactService.getDetailById(id).orElse(null);
+
+		model.addAttribute("detail", detail);
+
+		return "contactDetail";
+
+	}
 	
 
 }
