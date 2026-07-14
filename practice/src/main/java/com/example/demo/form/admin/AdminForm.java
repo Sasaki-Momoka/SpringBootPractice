7c971f0907a3ff120eb82a4bf2fa96e261f96221
Sasaki-Momoka <momosasa39@gmail.com>
2026-07-14 18:24:26 +0900

    progress４

--- practice/src/main/java/com/example/demo/form/admin/AdminForm.java
+++ practice/src/main/java/com/example/demo/form/admin/AdminForm.java
@@ -7,9 +7,9 @@ import jakarta.validation.constraints.Size;
 public class AdminForm {
 	
 	@NotBlank(message = "姓を入力してください")
-	private String last_name;
+	private String lastName;
 	@NotBlank(message = "姓を入力してください")
-	private String first_name;
+	private String firstName;
 	
 	@NotBlank(message = "メールアドレスを入力してください")
 	@Email(message = "正しいメールアドレスを入力してください")
@@ -19,10 +19,18 @@ public class AdminForm {
 	@Size(message = "パスワードは8文字以上で入力してください")
 	private String password;
 	
+    public String getLastName() {return lastName;}
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    
+    public String getFirstName() {return firstName;}
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+	
 	public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
+    
+
 }
 	
 
--- practice/src/main/java/com/example/demo/repository/admin/AdminRepository.java
+++ practice/src/main/java/com/example/demo/repository/admin/AdminRepository.java
@@ -1,10 +1,9 @@
-/*package com.example.demo.repository.admin;
+package com.example.demo.repository.admin;
 
 import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface AdminRepository extends JpaRepository<User, Long> {
   
-}*/
+}
